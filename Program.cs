6ac4ec676345d6269b7533d91fe9f1787a8d3bf1
Amans <notshashank10@gmail.com>
2023-07-13 09:57:24 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter marks in Pre: ");
+        int marksPre = Convert.ToInt32(Console.ReadLine());
+
+        if (marksPre > 60)
+        {
+            Console.Write("Enter marks in Final: ");
+            int marksFinal = Convert.ToInt32(Console.ReadLine());
+
+            if (marksFinal > 55)
+            {
+                Console.WriteLine("Selected");
+            }
+            else
+            {
+                Console.WriteLine("Fail in Final");
+            }
+        }
+        else if (marksPre <= 60)
+        {
+            Console.WriteLine("Fail in Pre");
+        }
+
+        Console.ReadLine();
+    }
+}
